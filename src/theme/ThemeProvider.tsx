60e4061a932ba6ed9fff7f6a3db4d250f24301dd
Violet Kiseleva <52976585+lastmarsch@users.noreply.github.com>
2022-09-15 13:04:46 +0300

    Add theme: context, provider, hook (#7)

---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,19 @@
+import { FC, useMemo, useState } from 'react'
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext'
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
+
+export const ThemeProvider: FC = ({ children }) => {
+    const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+    const defaultProps = useMemo(() => ({
+        theme,
+        setTheme
+    }), [theme])
+
+    return (
+        <ThemeContext.Provider value={defaultProps}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
---
+++ src/theme/useTheme.ts
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext";
+
+interface UseThemeResult {
+    toggleTheme: () => void;
+    theme: Theme;
+}
+
+export const useTheme = (): UseThemeResult => {
+    const { theme, setTheme } = useContext(ThemeContext)
+
+    const toggleTheme = () => {
+        const newTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT
+        setTheme(newTheme);
+        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
+    }
+
+    return {theme, toggleTheme};
+}
