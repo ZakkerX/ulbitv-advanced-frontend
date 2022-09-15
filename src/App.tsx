0a7b65ef7390d5187a9b8670b9663faaaba3d54d
Violet Kiseleva <52976585+lastmarsch@users.noreply.github.com>
2022-09-15 11:54:48 +0300

    Add lazy loading, suspense (#6)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { Counter } from './components/Counter';
+import { Suspense } from 'react';
+import { Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './index.scss'
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+
 
 export const App = () => {
   return (
     <div className='app'>
-        <Counter />
+      <Link to={'/'}>Main</Link>
+      <Link to={'/about'}>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync />} />
+          <Route path={'/'} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
---
+++ src/pages/AboutPage/AboutPage.async.tsx
@@ -0,0 +1,7 @@
+import { lazy } from "react";
+
+export const AboutPageAsync = lazy(() => new Promise(resolve => {
+    // @ts-ignore
+    // Test delay simulation
+    setTimeout(() => resolve(import('./AboutPage')), 1500)
+}))
