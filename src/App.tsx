b4523c48deda04e07c995d49320fd5ded1758db2
Violet <eerfeer@yandex.ru>
2022-09-15 12:57:50 +0300

    Add theme: context, provider, hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Suspense } from 'react';
 import { Link } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
-import './index.scss'
+import './styles/index.scss'
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
-
+import { useTheme } from './theme/useTheme';
 
 export const App = () => {
+  const {theme, toggleTheme} = useTheme();
+
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle theme</button>
       <Link to={'/'}>Main</Link>
       <Link to={'/about'}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
