4ba0e257c58d7716979a592ca5bbfec407fe5237
Violet Kiseleva <52976585+lastmarsch@users.noreply.github.com>
2022-09-22 23:19:26 +0300

    Add HMR (#16)

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -1,13 +1,13 @@
 import { BuildOptions } from "./types/config";
-import { Configuration as DevServerConfiguration } from 'webpack-dev-server';
+import { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
-export const buildDevServer = (options: BuildOptions): DevServerConfiguration => {
-    return {
-        port: options.port,
-        // automatically opens the page
-        open: true, 
-        // allows to proxy requests through a specified index page (by default 'index.html'), 
-        // useful for SPAs that utilise the HTML5 History API.
-        historyApiFallback: true,  
-    }
-}
+export const buildDevServer = (
+  options: BuildOptions
+): DevServerConfiguration => {
+  return {
+    port: options.port,
+    open: true,
+    historyApiFallback: true,
+    hot: true,
+  };
+};
