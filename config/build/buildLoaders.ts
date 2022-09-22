3672fa9443b01c8bf53b0a4cba9e307240be0c79
Violet <eerfeer@yandex.ru>
2022-09-22 23:46:41 +0300

    Add babel loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,52 +1,74 @@
-import MiniCssExtractPlugin from "mini-css-extract-plugin"
-import webpack from "webpack"
-import { BuildOptions } from "./types/config"
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
 
-export const buildLoaders = ({isDev}: BuildOptions): webpack.RuleSetRule[] => {
-    const svgLoader = {
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-    };
+export const buildLoaders = ({
+  isDev,
+}: BuildOptions): webpack.RuleSetRule[] => {
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ["@svgr/webpack"],
+  };
 
-    const fileLoader = {
-        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
-        use: [
-            {
-                loader: 'file-loader',
-            },
-        ]
-    }
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: "file-loader",
+      },
+    ],
+  };
 
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-            {
-                loader: "css-loader",
-                options: {
-                    modules: {
-                        auto: (resPath: string) => !!resPath.includes('.module.'),
-                        localIdentName: isDev 
-                        ? "[path][name]__[local]--[hash:base64:5]" 
-                        : "[hash:base64:8]"
-                    },                    
-                }
-            },
-            "sass-loader",
-        ],
-    }
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => !!resPath.includes(".module."),
+            localIdentName: isDev
+              ? "[path][name]__[local]--[hash:base64:5]"
+              : "[hash:base64:8]",
+          },
+        },
+      },
+      "sass-loader",
+    ],
+  };
 
-    // for native js & jsx babel is needed
-    const tsLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
+  //   const babelLoader = {
+  //     test: /\.(js|jsx|tsx)$/,
+  //     exclude: /(node_modules|bower_components)/,
+  //     use: {
+  //       loader: "babel-loader",
+  //       options: {
+  //         presets: ["@babel/preset-env"],
+  //         plugins: [
+  //           [
+  //             "i18next-extract",
+  //             {
+  //               locales: ["ru", "en", "ro"],
+  //               keyAsDefaultValue: true,
+  //             },
+  //           ],
+  //         ],
+  //       },
+  //     },
+  //   };
 
-    return [
-        svgLoader,
-        fileLoader,
-        cssLoader,
-        tsLoader,
-    ]
-}
+  const tsLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  return [
+    svgLoader,
+    fileLoader,
+    cssLoader,
+    // babelLoader,
+    tsLoader,
+  ];
+};
